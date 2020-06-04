6beb47c6854326a196424010f7e439ab03f7dccc
sunxulong4399 <1015435340@qq.com>
2020-06-04 14:32:55 +0800

    列表页bug修改

--- src/app/pages/product/product-routing.module.ts
+++ src/app/pages/product/product-routing.module.ts
@@ -7,9 +7,9 @@ import {ProductDetialComponent} from './product-detial/product-detial.component'
 
 const routes: Routes = [
   {path: '', redirectTo: 'product-category', pathMatch: 'full'},
-  {path: 'product-category', component: ProductCategoryComponent, data: {title: '商品种类',keep: true}},
-  {path: 'product-list', component: ProductListComponent, data: {title: '商品列表',keep: true}},
-  {path: 'product-detail', component: ProductDetialComponent, data: {title: '商品详情',keep: true}},
+  {path: 'product-category', component: ProductCategoryComponent, data: {title: '商品种类', keep: true}},
+  {path: 'product-list', component: ProductListComponent, data: {title: '商品列表', keep: true}},
+  {path: 'product-detail', component: ProductDetialComponent, data: {title: '商品详情', keep: true}},
 ];
 
 @NgModule({
