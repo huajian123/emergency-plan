3511d56fa234498d8edf5ec0fe7e6c30bcbe76b2
sunxulong4399 <1015435340@qq.com>
2020-07-14 15:50:33 +0800

    修改

--- src/app/pages/local-emergency/natural-disasters/drought-and-flood/first-drought-flood/first-drought-flood.component.ts
+++ src/app/pages/local-emergency/natural-disasters/drought-and-flood/first-drought-flood/first-drought-flood.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {PageTypeEnum} from '../../../../../core/vo-common/BusinessEnum';
 
 @Component({
     selector: 'app-first-drought-flood',
@@ -6,8 +7,14 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./first-drought-flood.component.less']
 })
 export class FirstDroughtFloodComponent implements OnInit {
+    currentPage: number;
+    pageTypeEnum = PageTypeEnum;
 
     constructor() {
+    }
+
+    returnToList() {
+        console.log(1);
     }
 
     ngOnInit(): void {
