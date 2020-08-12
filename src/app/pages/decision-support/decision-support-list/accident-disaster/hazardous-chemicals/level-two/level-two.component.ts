dc0954cc336a465d4a8a4a37a863017d6f1afcfd
huajian <287643967@qq.com>
2020-08-12 15:23:46 +0800

    添加六边形选中效果

--- src/app/pages/decision-support/decision-support-list/accident-disaster/hazardous-chemicals/level-two/level-two.component.ts
+++ src/app/pages/decision-support/decision-support-list/accident-disaster/hazardous-chemicals/level-two/level-two.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {NzMessageService} from 'ng-zorro-antd';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {fromEvent} from 'rxjs';
 
 @Component({
     selector: 'app-level-two',
@@ -7,9 +7,22 @@ import {NzMessageService} from 'ng-zorro-antd';
     styleUrls: ['./level-two.component.less']
 })
 export class LevelTwoComponent implements OnInit {
-    constructor(public message?: NzMessageService) {
+    constructor(public element: ElementRef, private renderer2: Renderer2) {
     }
+
     ngOnInit(): void {
+        const temp = this.element.nativeElement.getElementsByClassName('hexagon-item');
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < temp.length; i++) {
+            const mouseClick = fromEvent(temp[i], 'click');
+            const subscription = mouseClick.subscribe(() => {
+                // tslint:disable-next-line:prefer-for-of
+                for (let j = 0; j < temp.length; j++) {
+                    this.renderer2.removeClass(temp[j], 'hexagon-item-click');
+                }
+                this.renderer2.addClass(temp[i], 'hexagon-item-click');
+            });
+        }
     }
 
 }
