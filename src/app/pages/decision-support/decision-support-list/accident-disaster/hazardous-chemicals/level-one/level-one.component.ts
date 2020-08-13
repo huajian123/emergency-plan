d4aacb6763b3bba97b42a8359e8125c1f9525c4c
sunxulong4399 <1015435340@qq.com>
2020-08-13 09:38:18 +0800

    树状图部门结构，及弹窗样式一 弹窗样式二、和tab切换弹窗还未完成

--- src/app/pages/decision-support/decision-support-list/accident-disaster/hazardous-chemicals/level-one/level-one.component.ts
+++ src/app/pages/decision-support/decision-support-list/accident-disaster/hazardous-chemicals/level-one/level-one.component.ts
@@ -1,13 +1,45 @@
 import {Component, OnInit} from '@angular/core';
 
+
 @Component({
     selector: 'app-level-one',
     templateUrl: './level-one.component.html',
     styleUrls: ['./level-one.component.less']
 })
 export class LevelOneComponent implements OnInit {
+    isVisible = false;
+    isArray: any[];
+    isNum: number;
 
     constructor() {
+        this.isNum = null;
+        this.isArray = [false, false, false, false, false, false, false, false, false];
+    }
+
+    showModal(e): void {
+        this.isNum = e;
+        if (e == null) {
+            return;
+        }
+        switch (e) {
+            case 0:
+                this.isArray[0] = true;
+                break;
+            case 1:
+                this.isArray[1] = true;
+                break;
+            case 2:
+                this.isArray[2] = true;
+                break;
+            default:
+                return;
+        }
+        this.isVisible = true;
+    }
+
+    handleCancel(): void {
+        this.isVisible = false;
+        this.isArray[this.isNum] = false;
     }
 
     ngOnInit(): void {
